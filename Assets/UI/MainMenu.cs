e77b8abfa800c5b818b75db0cbc1fc0c99b32cfc
ItzAman53 <06amaangupta@gmail.com>
2026-06-25 00:44:13 +0530

    BUild

--- Assets/UI/MainMenu.cs
+++ Assets/UI/MainMenu.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject menuPanel;
+[SerializeField] private GameObject introPanel; 
+[SerializeField] private TextMeshProUGUI storyText;
+[SerializeField] private float typingSpeed = 0.03f;
     public void StartGame()
     {
-        SceneManager.LoadScene("LevelScene");
+        StartCoroutine(StartSequence());
     }
 
     public void QuitGame()
@@ -13,5 +19,49 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
 
         Debug.Log("Quit Game");
+    }
+    IEnumerator StartSequence()
+    {
+        menuPanel.SetActive(false);
+        introPanel.SetActive(true);
+
+        storyText.text = "";
+
+        yield return StartCoroutine(TypeText(
+            "You are an archaeologist."));
+
+        yield return new WaitForSeconds(1f);
+
+        storyText.text += "\n\n";
+
+        yield return StartCoroutine(TypeText(
+            "The cave was rumored to hold treasures untouched for centuries."));
+
+        yield return new WaitForSeconds(1f);
+
+        storyText.text += "\n\n";
+
+        yield return StartCoroutine(TypeText(
+            "Most who entered never returned."));
+
+        yield return new WaitForSeconds(1f);
+
+        storyText.text += "\n\n";
+
+        yield return StartCoroutine(TypeText(
+            "You came anyway."));
+
+        yield return new WaitForSeconds(2f);
+
+        SceneManager.LoadScene("LevelScene");
+    }
+
+    IEnumerator TypeText(string message)
+    {
+        foreach (char c in message)
+        {
+            storyText.text += c;
+            yield return new WaitForSeconds(typingSpeed);
+        }
     }
 }
